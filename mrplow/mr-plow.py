1eb67a63fa947e74445412a37eb83a594ed7bc04
Justin Ennen <justin.ennen@ennwise.com>
2024-11-22 01:21:49 -0800

    - Implement persistent storage for player data using a JSON file. - Added methods to load and save player data (`load_players` and `save_players`). - Scheduled a daily reset of players' HP and deaths (`schedule_reset` and `reset_players`). - Increased players' maximum HP based on their level. - Added periodic leaderboard announcements (`schedule_leaderboard_announcement` and `announce_leaderboard`). - Announced when a player moves up the leaderboard (`update_leaderboard`).

--- mrplow/mr-plow.py
+++ mrplow/mr-plow.py
@@ -1,19 +1,50 @@
 import random
 import irc.bot
 import irc.strings
+import json
+import os
+import time
+from threading import Timer
 from message_templates import message_templates
 
+DATABASE_FILE = "players.json"
+
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, level=1, hp=100, weapon='fists', armor='clothes', alive=True):
         self.name = name
-        self.level = 1
-        self.hp = 100
-        self.weapon = 'fists'
-        self.armor = 'clothes'
-        self.alive = True
+        self.level = level
+        self.hp = hp
+        self.weapon = weapon
+        self.armor = armor
+        self.alive = alive
+
+    @property
+    def max_hp(self):
+        return 100 + (self.level - 1) * 10
 
     def status(self):
-        return f"{self.name} level: {self.level}, hp: {self.hp}, weapon: '{self.weapon}', armor: '{self.armor}'"
+        return f"{self.name} level: {self.level}, hp: {self.hp}/{self.max_hp}, weapon: '{self.weapon}', armor: '{self.armor}'"
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "level": self.level,
+            "hp": self.hp,
+            "weapon": self.weapon,
+            "armor": self.armor,
+            "alive": self.alive
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Player(
+            name=data["name"],
+            level=data["level"],
+            hp=data["hp"],
+            weapon=data["weapon"],
+            armor=data["armor"],
+            alive=data["alive"]
+        )
 
 class NPC:
     def __init__(self, name):
@@ -21,13 +52,16 @@ class NPC:
         self.hp = 50
         self.weapon = 'claws'
         self.alive = True
-
+        
 class IRCBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
-        self.players = {}
+        self.players = self.load_players()
         self.npcs = {}
+        self.leaderboard = []
+        self.schedule_reset()
+        self.schedule_leaderboard_announcement()
 
     def on_welcome(self, connection, event):
         connection.join(self.channel)
@@ -39,11 +73,13 @@ class IRCBot(irc.bot.SingleServerIRCBot):
         for nick in event.arguments[2].split():
             if nick not in self.players:
                 self.players[nick] = Player(nick)
+                self.save_players()
 
     def on_join(self, connection, event):
         nick = event.source.nick
         if nick not in self.players:
             self.players[nick] = Player(nick)
+            self.save_players()
             connection.privmsg(self.channel, f"Welcome {nick}! You have been added to the game.")
 
     def on_pubmsg(self, connection, event):
@@ -67,12 +103,14 @@ class IRCBot(irc.bot.SingleServerIRCBot):
     def handle_status(self, connection, nick):
         if nick not in self.players:
             self.players[nick] = Player(nick)
+            self.save_players()
         player = self.players[nick]
         connection.privmsg(self.channel, f"{nick}: {player.status()}")
 
     def handle_fite(self, connection, nick, target):
         if nick not in self.players:
             self.players[nick] = Player(nick)
+            self.save_players()
         player = self.players[nick]
 
         if target.startswith("NPC_"):
@@ -90,24 +128,29 @@ class IRCBot(irc.bot.SingleServerIRCBot):
     def handle_weapon(self, connection, nick, weapon):
         if nick not in self.players:
             self.players[nick] = Player(nick)
+            self.save_players()
         player = self.players[nick]
         player.weapon = weapon
+        self.save_players()
         connection.privmsg(self.channel, f"{nick}: weapon set to {weapon}.")
 
     def handle_armor(self, connection, nick, armor):
         if nick not in self.players:
             self.players[nick] = Player(nick)
+            self.save_players()
         player = self.players[nick]
         player.armor = armor
+        self.save_players()
         connection.privmsg(self.channel, f"{nick}: armor set to {armor}.")
 
     def handle_revive_all(self, connection):
         for player in self.players.values():
-            player.hp = 100
+            player.hp = player.max_hp
             player.alive = True
         for npc in self.npcs.values():
             npc.hp = 50
             npc.alive = True
+        self.save_players()
         connection.privmsg(self.channel, "The good fairy has come along and revived everyone.")
 
     def combat(self, connection, player, opponent):
@@ -179,6 +222,51 @@ class IRCBot(irc.bot.SingleServerIRCBot):
             if opponent.hp <= 0:
                 opponent.alive = False
                 connection.privmsg(self.channel, f"{opponent.name} has been defeated by {player.name}.")
+
+        self.save_players()
+        self.update_leaderboard(connection)
+
+    def load_players(self):
+        if os.path.exists(DATABASE_FILE):
+            with open(DATABASE_FILE, "r") as f:
+                data = json.load(f)
+                return {name: Player.from_dict(player_data) for name, player_data in data.items()}
+        return {}
+
+    def save_players(self):
+        with open(DATABASE_FILE, "w") as f:
+            json.dump({name: player.to_dict() for name, player in self.players.items()}, f)
+
+    def schedule_reset(self):
+        now = time.time()
+        next_reset = now + (24 * 60 * 60) - (now % (24 * 60 * 60))
+        Timer(next_reset - now, self.reset_players).start()
+
+    def reset_players(self):
+        for player in self.players.values():
+            player.hp = player.max_hp
+            player.alive = True
+        self.save_players()
+        self.connection.privmsg(self.channel, "Mr. Plow has reset everyone's HP! Time to get back to fighting!")
+        self.schedule_reset()
+
+    def schedule_leaderboard_announcement(self):
+        Timer(3600, self.announce_leaderboard).start()  # Announce every hour
+
+    def announce_leaderboard(self):
+        sorted_players = sorted(self.players.values(), key=lambda p: p.level, reverse=True)
+        leaderboard = "\n".join([f"{i+1}. {player.name} (Level {player.level})" for i, player in enumerate(sorted_players[:10])])
+        self.connection.privmsg(self.channel, f"Mr. Plow's Leaderboard:\n{leaderboard}")
+        self.schedule_leaderboard_announcement()
+
+    def update_leaderboard(self, connection):
+        sorted_players = sorted(self.players.values(), key=lambda p: p.level, reverse=True)
+        new_leaderboard = [player.name for player in sorted_players[:10]]
+        if new_leaderboard != self.leaderboard:
+            for i, player_name in enumerate(new_leaderboard):
+                if player_name not in self.leaderboard:
+                    connection.privmsg(self.channel, f"Mr. Plow says: Congratulations {player_name}! You've moved up to position {i+1} on the leaderboard!")
+            self.leaderboard = new_leaderboard
 
 def determine_hit_degree(damage):
     if damage <= 5:
