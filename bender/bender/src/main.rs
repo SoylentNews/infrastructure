76c14caf55b362be6e23866718afbe8b5172f2b7
Justin Ennen <justin.ennen@ennwise.com>
2025-06-30 15:46:21 -0700

    fix: Remove unnecessary closing brace in main function

--- bender/bender/src/main.rs
+++ bender/bender/src/main.rs
@@ -103,7 +103,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             }
                         }
                     }
-                    }
                 }
             }
             // --- New handlers for PART and KICK ---
